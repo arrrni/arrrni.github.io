823ea27745b861be054541f43d36f06d9030e5d8
Artur Szymczyk <arturszymczyk1@gmail.com>
2018-11-14 10:52:51 +0100

    Updates

---
+++ precache-manifest.7170561f52f8e887abc74026dbd712f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bae537403ea65cadf1e0",
+    "url": "/static/js/main.bae53740.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "bae537403ea65cadf1e0",
+    "url": "/static/css/main.1c4d8a8c.chunk.css"
+  },
+  {
+    "revision": "1e1b4f2a92f7972aaec9ff6d4c9865f1",
+    "url": "/index.html"
+  }
+];
